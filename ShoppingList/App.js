406ba764315cbaa9f173bb6373285d25b6959206
Moy Macías <66700201+moymacias@users.noreply.github.com>
2022-11-03 11:43:09 -0600

    Add files via upload

---
+++ ShoppingList/App.js
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+
+import Modal from "./components/Modal";
+
+export default function App() {
+  const [textItem, setTextItem] = useState("");
+  const [list, setList] = useState([]);
+
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [itemSelected, setItemSelected] = useState({});
+
+  const onHandleChange = (t) => setTextItem(t);
+
+  const addItem = () => {
+    setList((currentState) => [
+      ...currentState,
+      { id: Math.random().toString(), value: textItem },
+    ]);
+    setTextItem("");
+    console.log("hola desde chrome");
+  };
+
+  const selectedItem = (id) => {
+    console.log(id);
+    //setItemSelected(list.filter((item) => item.id === id)[0]);
+    setItemSelected(list.find((item) => item.id === id));
+    setModalVisible(true);
+  };
+
+  const deleteItem = () => {
+    console.log(itemSelected);
+    setList((currentState) =>
+      currentState.filter((item) => item.id !== itemSelected.id)
+    );
+    setItemSelected({});
+    setModalVisible(false);
+  };
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => selectedItem(item.id)}>
+      <Text>{item.value}</Text>
+    </TouchableOpacity>
+  );
+
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize: 30 }}>Shopping List 🛍️</Text>
+      <View style={styles.inputcontainer}>
+        <TextInput
+          placeholder="new item"
+          placeholderTextColor="white"
+          style={styles.inputStyle}
+          value={textItem}
+          onChangeText={onHandleChange}
+        />
+        <TouchableOpacity style={styles.button} onPress={addItem}>
+          <Text> Add </Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <FlatList
+          data={list}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
+      <Modal isVisible={modalVisible} actionDeleteItem={deleteItem} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#344955",
+    alignItems: "center",
+    paddingTop: 100,
+  },
+  inputcontainer: {
+    marginTop: 30,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    width: "100%",
+    paddingHorizontal: 30,
+  },
+  inputStyle: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    width: 250,
+  },
+  button: {
+    backgroundColor: "#F9AA33",
+    height: 35,
+    width: 45,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 15,
+  },
+});
