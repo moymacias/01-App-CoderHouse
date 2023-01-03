88a2eee173fb2887da088c14d1e07a3ca90de45c
Moy Macías <moymacias@outlook.com>
2023-01-02 19:41:08 -0600

    update maps app

--- MapsApp/App.js
+++ MapsApp/App.js
@@ -3,6 +3,15 @@ import PlaceNavigator from "./src/navigation/PlaceNavigator";
 import { Provider } from "react-redux";
 import store from "./src/store";
 
+import { init } from "./src/db";
+
+init()
+  .then(() => console.log("Database initialized"))
+  .catch((err) => {
+    console.log("Data base fail connect");
+    console.log(err.message);
+  });
+
 export default function App() {
   return (
     <Provider store={store}>
--- MapsApp/src/store/places.actions.js
+++ MapsApp/src/store/places.actions.js
@@ -1,7 +1,9 @@
 import * as FileSystem from "expo-file-system";
 import Map from "../constants/Map";
+import { insertAddress, fetchAddress } from "../db";
 
 export const ADD_PLACE = "ADD_PLACE";
+export const LOAD_PLACES = "LOAD_PLACES";
 
 export const addPlace = (title, image, location) => {
   return async (dispatch) => {
@@ -20,8 +22,6 @@ export const addPlace = (title, image, location) => {
       );
 
     const address = resData.results[0].formatted_address;
-    console.log(address);
-    console.log(location)
 
     const fileName = image.split("/").pop();
     const Path = FileSystem.documentDirectory + fileName;
@@ -31,6 +31,14 @@ export const addPlace = (title, image, location) => {
         from: image,
         to: Path,
       });
+      const result = await insertAddress(
+        title,
+        Path,
+        address,
+        location.lat,
+        location.lng
+      );
+      console.log(result);
     } catch (err) {
       console.log(err.message);
       throw err;
@@ -45,5 +53,17 @@ export const addPlace = (title, image, location) => {
         lng: location.lng,
       },
     });
+  };
+};
+
+export const loadAddress = () => {
+  return async (dispatch) => {
+    try {
+      const result = await fetchAddress();
+      console.log(result);
+      dispatch({ type: LOAD_PLACES, places: result.rows._array });
+    } catch (error) {
+      throw err;
+    }
   };
 };
